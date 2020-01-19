0e18497b5bdba04a2f7694cfb42433ff4f266a33
sharifmuai@gmail.com <DELL@DESKTOP-VOTJT06.com>
2020-01-20 02:40:24 +0400

    Updates

---
+++ precache-manifest.7badae986d9be8040abeae4d93375c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "4690e2a324d666e42264",
+    "url": "/monsters-rolodex/static/js/main.a517bd39.chunk.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "4690e2a324d666e42264",
+    "url": "/monsters-rolodex/static/css/main.a1f0b1f3.chunk.css"
+  },
+  {
+    "revision": "39d9262c36bc7931ce5ac1ce0f1e6cc2",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
